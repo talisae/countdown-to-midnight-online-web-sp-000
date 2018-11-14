d415ee0819b5544e0a633c2af359d5eee9b74bc3
Talisa Erazo <talisaerazo@gmail.com>
2018-11-14 05:36:00 +0000

    automatically backed up by learn

--- countdown.rb
+++ countdown.rb
@@ -1,10 +1,10 @@
 #write your code here
 
 def countdown(number)
-  number = 12
+  number = 10
   while number > 0
-  puts "#{number} SECOND(S)!"
-  number -= 1
+    puts "#{number} SECOND(S)!"
+    number -= 1
   return "HAPPY NEW YEAR!"
   end
 end
